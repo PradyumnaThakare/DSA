046efc2e4be5c9cecce3be4335fb7b7ddefd2db3
Pradyumna Thakare <pradyumnathakare@Pradyumnas-Mac-mini.local>
2025-02-06 20:13:39 +0530

    Second

--- Basics/patterns/pattern5.cpp
+++ Basics/patterns/pattern5.cpp
@@ -3,18 +3,18 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<i<<" ";
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<"* ";
         }
         cout<<endl;
     }
 }
 /*
 5
-1 
-2 2 
-3 3 3 
-4 4 4 4 
-5 5 5 5 5 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
  */
